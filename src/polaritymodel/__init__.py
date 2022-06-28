a7bdf23bf74678371de5a351ed72de29085e32d0
Jordan Snyder <snydrew@gmail.com>
2022-06-28 15:37:54 +0200

    inc version number to correspond to new data fmt

--- src/polaritymodel/__init__.py
+++ src/polaritymodel/__init__.py
@@ -2,7 +2,7 @@
 Python package for running and visualizing the polarity model of organoid growth
 """
 
-__version__ = '0.0.5'
+__version__ = '0.1.0'
 
 # get main classes into top-level namespace
 from .polarcore import Polar, PolarWNT, run_and_save
